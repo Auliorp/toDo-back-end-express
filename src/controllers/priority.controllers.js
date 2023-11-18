05d84bb3ac064c019f56bebd58b3ca2acd1680b2
Auliorp <aulioroveroparedes@gmail.com>
2023-11-18 15:25:01 -0300

    Se creo el metodo GET por Id y Se realizaron las respectivas validaciones tanto en el GETId como en el PUT faltante

--- src/controllers/priority.controllers.js
+++ src/controllers/priority.controllers.js
@@ -48,7 +48,29 @@ export const putPriorityControllers = async (req, res) => {
       const { id } = req.params;
       const { title } = req.body;
 
+      const existingPriority = await Priority.findOne({
+         where: {
+            title,
+         },
+      });
+      if (isNaN(id)) {
+         return res
+            .status(400)
+            .json({ message: "el Id ingresado solo debe contener numeros" });
+      }
+      //Si existe un título igual y no pertenece a la misma priority que estamos editando, devuelve un error
+      if (existingPriority && existingPriority.id != id) {
+         return res.status(400).json({
+            message:
+               "El título que intentas asignar ya existe, por favor intente con uno diferente",
+         });
+      }
       const priority = await Priority.findByPk(id);
+      if (!priority) {
+         return res
+            .status(404)
+            .json({ message: "La prioridad que intentas editar no existe" });
+      }
       //se define la seccion que se modificara en este caso es title.
       priority.title = title;
       //Se guardan los cambios editados con save()
@@ -86,6 +108,23 @@ export const deletePriorityControllers = async (req, res) => {
    }
 };
 
-export const getPriorityIDControllers = (req, res) => {
-   res.send("Prioridad por id");
+export const getPriorityIDControllers = async (req, res) => {
+   try {
+      const { id } = req.params;
+      //isNaN(id) verifica si el id no es un numero
+      if (isNaN(id)) {
+         return res
+            .status(400)
+            .json({ message: "el Id ingresado solo debe contener numeros" });
+      }
+      const priorityId = await Priority.findByPk(id);
+      if (!priorityId) {
+         return res.status(404).json({
+            message: "Id ingresado no existe",
+         });
+      }
+      return res.status(200).json(priorityId);
+   } catch (error) {
+      return res.status(500).json({ message: error.message });
+   }
 };
